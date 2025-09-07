317074ed41ac1c7a4daf89884cb737c68c3d9bc9
devbydenis <hello.denisrahmadi@gmail.com>
2025-09-07 16:18:36 +0700

    intialize project

---
+++ src/main/java/com/sinaukoding/taskspringboot/day3/MainApp.java
@@ -0,0 +1,13 @@
+package com.sinaukoding.taskspringboot.day3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
+
+}
